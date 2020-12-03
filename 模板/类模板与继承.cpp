5b3f2f5a9cca5e7e8ee9c53871eab5205b1c58b6
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-12-03 19:24:11 +0800

    update

---
+++ 模板/类模板与继承.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+// 类模板与继承
+template<class T>
+class Base
+{
+	T m_t;
+};
+
+//class Son :public Base// 错误必须要知道父类中 T 的类型才能继承给子类
+class Son1 :public Base<int>
+{
+
+};
+
+// 子类也变成一个类模板
+template<class T1, class T2>
+class Son2 :public Base<T2>
+{
+public:
+	Son2()
+	{
+		cout << "T1 Type:" << typeid(T1).name() << endl;
+		cout << "T2 Type:" << typeid(T2).name() << endl;
+	}
+
+	T1 obj;
+};
+
+int main()
+{
+	Son1 s1; // 此时 Base的 T 只能是 int 类型
+
+	Son2<int, char> s2; // 此时 Base 的 T 是 char 类型
+
+
+	system("pause");
+	return 0;
+}
--- 模板/类模板对象做函数参数.cpp
+++ 模板/类模板对象做函数参数.cpp
@@ -16,7 +16,7 @@ public:
 
 	void ShowPerson()
 	{
-		cout << "Name: " << this->m_Name << " , Age: " << this->m_Age << endl;
+		cout << "Name: " << this->m_Name << "\tAge: " << this->m_Age << endl;
 	}
 
 public:
@@ -24,7 +24,7 @@ public:
 	T2 m_Age;
 };
 
-
+// 指定传入类型
 void printPerson01(Person<string, int> &p)
 {
 	p.ShowPerson();
@@ -34,16 +34,24 @@ template<class T1, class T2>
 void printPerson02(Person<T1, T2> &p)
 {
 	p.ShowPerson();
-	cout << "T1:" << typeid(T1).name() << endl;
-	cout << "T2:" << typeid(T2).name() << endl;
+	cout << "T1 Type:" << typeid(T1).name() << endl;
+	cout << "T2 Type:" << typeid(T2).name() << endl;
 }
-
-int main()
+// 整个类模板化
+template<class T>
+void printPerson03(T &p)
 {
-	Person<string, int> p1("Tom", 18);
-	//printPerson01(p1);
-	printPerson02(p1);
-
-	system("pause");
-	return 0;
+	p.ShowPerson();
+	cout << "T Type:" << typeid(T).name() << endl;
 }
+
+//int main()
+//{
+//	Person<string, int> p1("Tom", 18);
+//	//printPerson01(p1);
+//	//printPerson02(p1);
+//	printPerson03(p1);
+//
+//	system("pause");
+//	return 0;
+//}
